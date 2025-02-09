18f087def7bffb9677222414e3a9a62af1eaaed0
Anthony Champagne <dev@anthonychampagne.fr>
2025-02-09 01:10:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# unbound-exporter container
+
+![GitHub License](https://img.shields.io/github/license/anthochamp/container-unbound-exporter?style=for-the-badge)
+
+![GitHub Release](https://img.shields.io/github/v/release/anthochamp/container-unbound-exporter?style=for-the-badge&color=457EC4)
+![GitHub Release Date](https://img.shields.io/github/release-date/anthochamp/container-unbound-exporter?style=for-the-badge&display_date=published_at&color=457EC4)
+
+See the [container description](CONTAINER.md) for more informations.
---
+++ src/rootfs/docker-entrypoint.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env sh
+set -eu
+
+# shellcheck disable=SC2120
+replaceEnvSecrets() {
+	# replaceEnvSecrets 1.0.0
+	local prefix="${1:-}"
+
+	for envSecretName in $(export | awk '{print $2}' | grep -oE '^[^=]+' | grep '__FILE$'); do
+		if [ -z "$prefix" ] || printf '%s' "$envSecretName" | grep "^$prefix" >/dev/null; then
+			local envName
+			envName=$(printf '%s' "$envSecretName" | sed 's/__FILE$//')
+
+			local filePath
+			filePath=$(eval echo '${'"$envSecretName"':-}')
+
+			if [ -n "$filePath" ]; then
+				if [ -f "$filePath" ]; then
+					echo Using content from "$filePath" file for "$envName" environment variable value.
+
+					export "$envName"="$(cat -A "$filePath")"
+					unset "$envSecretName"
+				else
+					echo ERROR: Environment variable "$envSecretName" is defined but does not point to a regular file. 1>&2
+					exit 1
+				fi
+			fi
+		fi
+	done
+}
+
+replaceEnvSecrets UNBOUND_EXPORTER_
+
+UNBOUND_EXPORTER_HOST=${UNBOUND_EXPORTER_HOST:-}
+UNBOUND_EXPORTER_CA_FILE=${UNBOUND_EXPORTER_CA_FILE:-}
+UNBOUND_EXPORTER_CERT_FILE=${UNBOUND_EXPORTER_CERT_FILE:-}
+UNBOUND_EXPORTER_CERT_KEY_FILE=${UNBOUND_EXPORTER_CERT_KEY_FILE:-}
+
+if [ -n "${1:-}" ]; then
+	exec "$@"
+fi
+
+args=
+
+if [ -n "$UNBOUND_EXPORTER_HOST" ]; then
+	args=$args" -unbound.host '$UNBOUND_EXPORTER_HOST'"
+fi
+
+if [ -n "$UNBOUND_EXPORTER_CA_FILE" ]; then
+	args=$args" -unbound.ca '$UNBOUND_EXPORTER_CA_FILE'"
+fi
+
+if [ -n "$UNBOUND_EXPORTER_CERT_FILE" ]; then
+	args=$args" -unbound.cert '$UNBOUND_EXPORTER_CERT_FILE'"
+fi
+
+if [ -n "$UNBOUND_EXPORTER_CERT_KEY_FILE" ]; then
+	args=$args" -unbound.key '$UNBOUND_EXPORTER_CERT_KEY_FILE'"
+fi
+
+eval "exec /unbound_exporter$args"
